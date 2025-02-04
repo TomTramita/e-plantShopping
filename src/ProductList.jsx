07cd06af07434a2d7897cd2a4e9eee28bfe707e6
TomTramita <tramita.tom@gmail.com>
2025-02-04 09:32:13 -0500

    Navbar Facelift

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -268,30 +268,7 @@ function ProductList() {
     }));
   };
 
-  // Navbar styles
-  const styleObj = {
-    backgroundColor: '#4CAF50',
-    color: '#fff!important',
-    padding: '15px',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    fontSize: '20px',
-  };
-
-  const styleObjUl = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '1100px',
-  };
-
-  const styleA = {
-    color: 'white',
-    fontSize: '30px',
-    textDecoration: 'none',
-  };
-
+  
   return (
     <div>
       {/* Navbar */}
@@ -301,8 +278,8 @@ function ProductList() {
             <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
             <a href="/" style={{ textDecoration: 'none' }}>
               <div>
-                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
+                <h3 className="tag_home_link_h3">Paradise Nursery</h3>
+                <i >Where Green Meets Serenity</i>
               </div>
             </a>
           </div>
@@ -337,7 +314,7 @@ function ProductList() {
                   ></path>
                 </svg>
                 {/* Display the total number of items in the cart */}
-                <span className="cart-count">{totalItemsInCart}</span>
+                <span className="cart_quantity_count">{totalItemsInCart}</span>
               </h1>
             </a>
           </div>
